57f6703e9d2390df12d4161a9a5db3e248847659
Zen Trias Aji Saputra <zentryas27@gmail.com>
2025-04-22 16:20:54 +0700

    Update env and config services

--- config/services.php
+++ config/services.php
@@ -30,4 +30,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_DRIVE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+        'refresh_token' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+        'folder_id' => env('GOOGLE_DRIVE_FOLDER_ID'),
+    ],
+
 ];
